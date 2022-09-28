ef055b38bf5548db0aa240450a2d166ae3251c6c
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-09-28 13:58:10 -0400

    commit tree 7c32a6ee88c17c61ad4bf9c794c4778eaac2ebb0

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 22, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 11, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 5, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 9, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 99, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 24, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 1, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 71, "M", "black", "aggressive", 0);
